64e3cdba55dba149ade70000599354db5aa9dbab
dobrodeev <dobrod@meta.ua>
2018-06-30 20:50:38 +0300

    Написали запрос RIGHT JOIN чтобы вывести пользователя и все его сообщения

--- userAndMessage.php
+++ userAndMessage.php
@@ -16,6 +16,9 @@ require_once 'connectDB.php';
 $q = 'SELECT surname FROM users RIGHT OUTER JOIN messages ON users.id_user = messages.id_user';
 $all = mysqli_query($connect, $q);
 $all_message = mysqli_fetch_array($all, MYSQLI_ASSOC);
+echo '<pre>';
+print_r($all_message);
+echo '</pre>';
 ?>
 </body>
 </html>

d487d4546efab3e8be4be68f5df32db98e47fc33
dobrodeev <dobrod@meta.ua>
2018-07-04 14:14:24 +0300

    написали запрос для вывода всех пользователей и их месседжей

--- userAndMessage.php
+++ userAndMessage.php
@@ -14,12 +14,24 @@
 <?php
 require_once 'connectDB.php';
 //$q = 'SELECT surname FROM users RIGHT OUTER JOIN messages ON users.id_user = messages.id_user';
-$queryJOIN = "SELECT users.surname, messages.message FROM users RIGHT JOIN messages ON users.id_user = messages.id_user";
-$all = mysqli_query($connect, $queryJOIN);
-$all_message = mysqli_fetch_array($all, MYSQLI_ASSOC);
-echo '<pre>';
-print_r($all_message);
-echo '</pre>';
+//$queryJOIN = "SELECT users.surname, messages.message FROM users RIGHT JOIN messages ON users.id_user = messages.id_user";
+$queryAllMessages = "SELECT DISTINCT surname FROM messages";
+$all = mysqli_query($connect, $queryAllMessages);
+//$all_message = mysqli_fetch_array($all, MYSQLI_ASSOC);
+//echo '<pre>';
+//print_r($all_message);
+//echo '</pre>';
+$rows = mysqli_num_rows($all);
+echo 'Всего строк: '.$rows.'<br>';
+print_r($all);
+//while ($row = mysqli_fetch_row($all))
+//{
+//    for ($j = 0; $j < $rows; ++$j)
+//    {
+//        echo $row[$j].'<br>';
+//    }
+//}
+mysqli_free_result($all);
 ?>
 </body>
 </html>
